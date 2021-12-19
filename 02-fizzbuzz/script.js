fe9ed929a292c707c3b0ac97b45330bb049a4b6c
SmthOfValue <bik00014@gmail.com>
2021-12-19 12:00:28 +0300

    solved fizzbuzz

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -11,9 +11,28 @@
 */
 
 function fizzBuzz(num) {
-    // Напишите код здесь
+    for (let i = 1;i<=num;i++){
+        if (i%3 === 0) {
+            if (i%5 === 0){
+                console.log('fizzbuzz');
+                continue;
+            }
+            else {
+                console.log('fizz');
+                continue;
+            }
+        }
+        if (i%5 === 0) {
+            console.log('buzz');
+            continue;
+        }
+        console.log(i);
+    }
+    
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(fizzBuzz(15));
+fizzBuzz(20);
+fizzBuzz(30);
